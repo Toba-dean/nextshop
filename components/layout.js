cbc9a2bd977202f1efcd2a2141779452a56eb974
Toba-dean <tobaogundimu@gmail.com>
2023-01-21 15:18:55 +0100

    added the script to make SSR of material ui elements, also changed the BG color of the mavbar

--- components/layout.js
+++ components/layout.js
@@ -1,24 +1,28 @@
 import Head from "next/head";
-import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import { AppBar, Container, Toolbar, Typography } from "@material-ui/core";
+import { useStyles } from "../utils/styles";
 
 export default function LayOut({ children }) {
+
+  const classes = useStyles();
+
   return (
     <>
       <Head>
         <title>Next Amazona</title>
       </Head>
 
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
           <Typography>amazona</Typography>
         </Toolbar>
       </AppBar>
 
-      <Container>
+      <Container className={classes.main}>
         {children}
       </Container>
 
-      <footer>
+      <footer className={classes.footer}>
         All rights reserved. Next Amazona.
       </footer>
     </>
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,17 @@
-import '../styles/globals.css'
+import { useEffect } from "react";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+
+  // fixing SSR of material ui elements
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
 
